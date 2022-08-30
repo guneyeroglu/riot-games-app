53c1c5e980209b47b3c4e0dc864427d48b4dc4fc
guneyeroglu <benguneyeroglu@gmail.com>
2022-08-30 21:17:52 +0300

    updated: fetch for lol char

--- src/pages/Lol/Champions/Champions.tsx
+++ src/pages/Lol/Champions/Champions.tsx
@@ -1,15 +1,50 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
+
+import axios from 'axios';
+
+import { CardLolChar } from '../../../components/Cards';
+
+import championsBackground from '../../../assets/images/lol/champions-background.jpeg';
+import styles from './champions.module.scss';
 
 const Champions = () => {
   const { t, i18n } = useTranslation();
+  const [champData, setChampData] = useState<any>([]);
 
   useEffect(() => {
     document.title = t('pageLolChampions');
     document.documentElement.lang = i18n.language.slice(0, 2);
+
+    const fetchLolData = async () => {
+      const response = await axios.get(
+        `https://ddragon.leagueoflegends.com/cdn/12.8.1/data/${i18n.language}/champion.json`
+      );
+
+      console.log('hey');
+
+      setChampData(response.data.data);
+    };
+
+    fetchLolData();
   }, [t, i18n]);
 
-  return <div>champ Champ</div>;
+  return (
+    <div className={styles.container}>
+      <div
+        className={styles.container__background}
+        style={{
+          backgroundImage: `url(${championsBackground})`,
+        }}
+      ></div>
+      <div className={styles.container__filter}></div>
+      <div className={styles.container__content}>
+        {Object.keys(champData).map((champName: string) => (
+          <CardLolChar data={champData[champName]} key={champName} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Champions;
