e28005b57a3a1ecf37ffec8cb94d269beb5b47af
guneyeroglu <benguneyeroglu@gmail.com>
2022-08-30 14:59:48 +0300

    updated: big changes

--- src/components/FeaturedTitle/FeaturedTitle.tsx
+++ src/components/FeaturedTitle/FeaturedTitle.tsx
@@ -1,14 +1,15 @@
 import { useTranslation } from 'react-i18next';
+
 import styles from './featured-title.module.scss';
 
-type types = 'champions' | 'regions';
+import { typesLol } from '../../global/types/TypesLol';
 
 interface IProps {
-  type: types;
+  type: typesLol;
 }
 
 interface IContent {
-  name: types;
+  name: typesLol;
   link: string;
   translation: string;
 }
@@ -26,11 +27,23 @@ const FeaturedTitle = (props: IProps) => {
       link: 'https://universe.leagueoflegends.com/esimages/content_type_icon_faction__14mjH.png',
       translation: 'featuredRegions',
     },
+    {
+      name: 'items',
+      link: 'https://universe.leagueoflegends.com/esimages/content_type_icon_trending__2Bld0.png',
+      translation: 'items',
+    },
+    {
+      name: 'ranks',
+      link: 'https://universe.leagueoflegends.com/esimages/content_type_icon_latest__1ulWu.png',
+      translation: 'ranks',
+    },
   ];
 
   enum FEATURED_TYPES {
     champions = 0,
     regions = 1,
+    items = 2,
+    ranks = 3,
   }
 
   const content = contents[FEATURED_TYPES[props.type]];
@@ -41,15 +54,19 @@ const FeaturedTitle = (props: IProps) => {
         <img src={content.link} alt={content.name} />
       </div>
       <div className={styles.wrapper__title}>
-        <img
-          src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAARcAAAABCAYAAAD0K+rmAAAA80lEQVR4AV2TiW0EMQwDh72loqSTFGwF8A0Exnu/xW/pc35/vr5DmBlIBvyRwP3I3BnctfC5XAMy5LOQC8ZBcZO7UNwBQUQr/cUHNNVMDYaLxhHwZpvmiJqE9l3pJOKfmYQX2zmTUPerj5jtBRKVBBl0dNjf97Hft99jl29vynHucPUGzN55txnS3U3XHHH/feh96WzH5CcJi1c0Jf70OJj3s65mwfXzc/s527ncxnbWaNkz+6p9XE5zJYoB7HQ3IHbQRa/2q9P59Nl7qrfmbzf9HwI0Kq6Dx9f55hS0fejkedg+BSjs+XvP5NM/67k4ATr/AU/oIFuGMAGgAAAAAElFTkSuQmCC'
-          alt='-'
-        />
+        <span>
+          <img
+            src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAARcAAAABCAYAAAD0K+rmAAAA80lEQVR4AV2TiW0EMQwDh72loqSTFGwF8A0Exnu/xW/pc35/vr5DmBlIBvyRwP3I3BnctfC5XAMy5LOQC8ZBcZO7UNwBQUQr/cUHNNVMDYaLxhHwZpvmiJqE9l3pJOKfmYQX2zmTUPerj5jtBRKVBBl0dNjf97Hft99jl29vynHucPUGzN55txnS3U3XHHH/feh96WzH5CcJi1c0Jf70OJj3s65mwfXzc/s527ncxnbWaNkz+6p9XE5zJYoB7HQ3IHbQRa/2q9P59Nl7qrfmbzf9HwI0Kq6Dx9f55hS0fejkedg+BSjs+XvP5NM/67k4ATr/AU/oIFuGMAGgAAAAAElFTkSuQmCC'
+            alt='-'
+          />
+        </span>
         <span>{t(content.translation)}</span>
-        <img
-          src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAARcAAAABCAYAAAD0K+rmAAAA80lEQVR4AV2TiW0EMQwDh72loqSTFGwF8A0Exnu/xW/pc35/vr5DmBlIBvyRwP3I3BnctfC5XAMy5LOQC8ZBcZO7UNwBQUQr/cUHNNVMDYaLxhHwZpvmiJqE9l3pJOKfmYQX2zmTUPerj5jtBRKVBBl0dNjf97Hft99jl29vynHucPUGzN55txnS3U3XHHH/feh96WzH5CcJi1c0Jf70OJj3s65mwfXzc/s527ncxnbWaNkz+6p9XE5zJYoB7HQ3IHbQRa/2q9P59Nl7qrfmbzf9HwI0Kq6Dx9f55hS0fejkedg+BSjs+XvP5NM/67k4ATr/AU/oIFuGMAGgAAAAAElFTkSuQmCC'
-          alt='-'
-        />
+        <span>
+          <img
+            src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAARcAAAABCAYAAAD0K+rmAAAA80lEQVR4AV2TiW0EMQwDh72loqSTFGwF8A0Exnu/xW/pc35/vr5DmBlIBvyRwP3I3BnctfC5XAMy5LOQC8ZBcZO7UNwBQUQr/cUHNNVMDYaLxhHwZpvmiJqE9l3pJOKfmYQX2zmTUPerj5jtBRKVBBl0dNjf97Hft99jl29vynHucPUGzN55txnS3U3XHHH/feh96WzH5CcJi1c0Jf70OJj3s65mwfXzc/s527ncxnbWaNkz+6p9XE5zJYoB7HQ3IHbQRa/2q9P59Nl7qrfmbzf9HwI0Kq6Dx9f55hS0fejkedg+BSjs+XvP5NM/67k4ATr/AU/oIFuGMAGgAAAAAElFTkSuQmCC'
+            alt='-'
+          />
+        </span>
       </div>
     </div>
   );
--- src/pages/Lol/LolMain/LolMain.tsx
+++ src/pages/Lol/LolMain/LolMain.tsx
@@ -6,22 +6,17 @@ import { motion } from 'framer-motion';
 
 import FeaturedTitle from '../../../components/FeaturedTitle/FeaturedTitle';
 
-import { images } from '../../../components/Images/';
+import { imagesCarousel, imagesOther } from '../../../components/Images/';
+
 import backgroundImageChampions from '../../../assets/images/lol/map-bg-1.jpeg';
 import backgroundImageRegions from '../../../assets/images/lol/map-bg-2.jpeg';
 
 import styles from './lol-main.module.scss';
 
-interface IImage {
-  id: number;
-  name: string;
-  url: string;
-}
-
 const LolMain = () => {
   const { t, i18n } = useTranslation();
 
-  const slider = useRef<HTMLDivElement | any>(null);
+  const carousel = useRef<HTMLDivElement | any>(null);
 
   const [width, setWidth] = useState<number>(0);
 
@@ -29,8 +24,8 @@ const LolMain = () => {
     document.title = t('pageLolHome');
     document.documentElement.lang = i18n.language.slice(0, 2);
 
-    const scrollWidth = slider.current?.scrollWidth;
-    const offsetWidth = slider.current?.offsetWidth;
+    const scrollWidth = carousel.current?.scrollWidth;
+    const offsetWidth = carousel.current?.offsetWidth;
 
     setWidth(scrollWidth - offsetWidth);
   }, [t, i18n]);
@@ -38,17 +33,17 @@ const LolMain = () => {
   return (
     <div className={styles.container}>
       <motion.div
-        ref={slider}
+        ref={carousel}
         whileTap={{ cursor: 'grabbing' }}
         whileHover={{ cursor: 'grab' }}
-        className={styles.container__slider}
+        className={styles.container__carousel}
       >
         <motion.div
           drag={'x'}
           dragConstraints={{ right: 0, left: -width }}
-          className={styles['container__slider--inner']}
+          className={styles['container__carousel--inner']}
         >
-          {images.map((image: IImage) => (
+          {imagesCarousel.map((image) => (
             <motion.div key={image.id} className={styles.item}>
               <img src={image.url} alt={image.name} />
             </motion.div>
@@ -62,13 +57,11 @@ const LolMain = () => {
             style={{ backgroundImage: `url(${backgroundImageChampions})` }}
           ></div>
           <div className={styles.champions__wrapper}>
-            <div className={styles['champions__wrapper--title']}>
+            <div className={styles.title}>
               <FeaturedTitle type='champions' />
             </div>
-            <div className={styles['champions__wrapper--content']}>
-              CARD ITEM
-            </div>
-            <div className={styles['champions__wrapper--nav']}>
+            <div className={styles.content}>CARD ITEM</div>
+            <div className={styles.nav}>
               <Link to='champions'>
                 <span>{t('viewChamps')}</span>
               </Link>
@@ -81,13 +74,11 @@ const LolMain = () => {
             style={{ backgroundImage: `url(${backgroundImageRegions})` }}
           ></div>
           <div className={styles.regions__wrapper}>
-            <div className={styles['regions__wrapper--title']}>
+            <div className={styles.title}>
               <FeaturedTitle type='regions' />
             </div>
-            <div className={styles['regions__wrapper--content']}>
-              Dizayn ve düşün
-            </div>
-            <div className={styles['regions__wrapper--nav']}>
+            <div className={styles.content}>Dizayn ve düşün</div>
+            <div className={styles.nav}>
               <Link to='regions'>
                 <span>{t('viewRegions')}</span>
               </Link>
@@ -95,7 +86,27 @@ const LolMain = () => {
           </div>
         </div>
       </div>
-      <div className={styles.container__nav}></div>
+      <div className={styles.container__others}>
+        {imagesOther.map((image) => (
+          <div className={styles.links} key={image.id}>
+            <div className={styles.links__wrapper}>
+              <div className={styles.title}>
+                <FeaturedTitle type={image.name} />
+              </div>
+              <div className={styles.content}>
+                <Link to={image.name}>
+                  <img src={image.url} alt={image.name} />
+                </Link>
+              </div>
+              <div className={styles.nav}>
+                <Link to={image.name}>
+                  <span>{t(image.view)}</span>
+                </Link>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
