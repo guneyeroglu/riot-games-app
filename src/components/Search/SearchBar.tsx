b81ce5d10beb39a7472cfb45a00761537cd4f5b7
guneyeroglu <benguneyeroglu@gmail.com>
2022-09-12 01:48:54 +0300

    updated: logo logic changed

--- src/components/Search/SearchBar.tsx
+++ src/components/Search/SearchBar.tsx
@@ -22,10 +22,17 @@ const SearchBar = (props: IProps) => {
   const placeholder: string =
     find === 'champion' ? t('searchChampion') : t('searchAgent');
 
-  const url: string =
-    find === 'champion'
-      ? 'https://universe.leagueoflegends.com/esimages/content_type_icon_champion__3nwJQ.png'
-      : '';
+  const logo: JSX.Element =
+    find === 'champion' ? (
+      <img
+        src={
+          'https://universe.leagueoflegends.com/esimages/content_type_icon_champion__3nwJQ.png'
+        }
+        alt='search-icon'
+      />
+    ) : (
+      <Icon name='ValorantIcon' />
+    );
 
   const handleInputChange = (event: { target: HTMLInputElement }) => {
     onSetInputValue(event.target.value.toLocaleLowerCase().toUpperCase());
@@ -38,13 +45,13 @@ const SearchBar = (props: IProps) => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.wrapper__filter}>
-        <img src={url} alt='search-icon' />
         <input
           type={'search'}
           placeholder={placeholder}
           value={inputValue}
           onChange={handleInputChange}
         />
+        <div className={styles['wrapper__filter--logo']}>{logo}</div>
         {inputValue.length > 0 && (
           <button
             className={styles['wrapper__filter--close']}
