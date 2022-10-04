622984d07497ed0e3c48f38aaa34f67e2b92834c
guneyeroglu <benguneyeroglu@gmail.com>
2022-10-04 23:12:16 +0300

    updated: color prop added

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -4,17 +4,21 @@ import styles from './spinner.module.scss';
 
 interface IProps {
   padding?: boolean;
+  color?: string;
 }
 
 const Spinner = (props: IProps) => {
-  const padding = props.padding;
+  const { padding, color } = props;
+
   const classList = padding
     ? `${styles.wrapper} ${styles.padding}`
     : styles.wrapper;
 
+  const sx = color ? { sx: { color: color } } : {};
+
   return (
     <div className={classList}>
-      <CircularProgress color='error' />
+      <CircularProgress color='error' {...sx} />
     </div>
   );
 };
