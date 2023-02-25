6eb14663836a817865191bbe378370c1e65ad207
guneyeroglu <benguneyeroglu@gmail.com>
2023-02-26 02:12:56 +0300

    updated: header responsive desing is done

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,12 @@
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 
-import Dropdown from '../Dropdown/Dropdown';
+import { useMediaQuery } from '@mui/material';
+
+import DrawerHeader from '../Drawers/DrawerHeader/DrawerHeader';
+import CustomMenu from '../CustomMenu/CustomMenu';
 import { Icon } from '../Icons/Icon';
 
 import styles from './header.module.scss';
@@ -22,8 +26,16 @@ interface ICountry {
 
 const Header = () => {
   const navigate = useNavigate();
-
+  const [open, setOpen] = useState<boolean>(false);
   const { t, i18n } = useTranslation();
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
 
   const handleNavigate = (pathname: string) => {
     navigate(pathname);
@@ -49,8 +61,8 @@ const Header = () => {
   }
 
   const listItems: INavItem[] = [
-    { id: 1, name: 'League of Legends', onClick: () => handleNavigate('/leagueoflegends') },
-    { id: 2, name: 'Valorant', onClick: () => handleNavigate('/valorant') },
+    { id: 1, name: NavName[0], onClick: () => handleNavigate('/leagueoflegends') },
+    { id: 2, name: NavName[1], onClick: () => handleNavigate('/valorant') },
   ];
 
   const countryList: ICountry[] = [
@@ -73,28 +85,74 @@ const Header = () => {
     localStorage.setItem('language', lang);
   };
 
+  const isMobile = useMediaQuery('(max-width: 768px)');
+
+  useEffect(() => {
+    if (isMobile) {
+      setOpen(false);
+    }
+
+    return () => {
+      setOpen(false);
+    };
+  }, [isMobile]);
+
   return (
     <header className={styles.wrapper}>
       <nav className={styles.wrapper__nav}>
         <ul>
-          <li>
-            <Dropdown items={listItems} input={<Icon name='RiotGamesIcon' wh={85} hg={27} vb={'0 0 587.93 165'} />} pointer clickAway />
-          </li>
+          {isMobile && (
+            <li>
+              <button className={styles.hamburger} onClick={handleDrawerOpen}>
+                <Icon name='RiotGamesIcon' wh={85} hg={27} vb={'0 0 587.93 165'} />
+                <Icon name='MenuArrowIcon' />
+              </button>
+            </li>
+          )}
+          {!isMobile && (
+            <li>
+              <CustomMenu items={listItems} input={<Icon name='RiotGamesIcon' wh={85} hg={27} vb={'0 0 587.93 165'} />} pointer clickAway />
+            </li>
+          )}
           <li className={styles.home}>
             <Link to='/'>
               <Icon name='RiotFistIcon' vb='0 0 32 32' />
             </Link>
           </li>
-          <li>
-            {allNavItems.map((navItem, idx) => (
-              <Dropdown key={idx} items={navItem} input={<span>{NavName[idx]}</span>} hover pointer clickAway />
-            ))}
-          </li>
+          {!isMobile && (
+            <li>
+              {allNavItems.map((navItem, idx) => (
+                <CustomMenu key={idx} items={navItem} input={<span>{NavName[idx]}</span>} hover pointer clickAway />
+              ))}
+            </li>
+          )}
         </ul>
       </nav>
-      <div className={styles.wrapper__settings}>
-        <Dropdown input={<Icon name='WorldIcon' wh={20} hg={20} />} items={countryList} pointer />
+      <div className={`${styles.wrapper__settings} ${isMobile ? styles.mobile : ''}`.trim()}>
+        <CustomMenu items={countryList} input={<Icon name='WorldIcon' />} />
       </div>
+      <DrawerHeader
+        open={open}
+        onClose={handleDrawerClose}
+        contents={allNavItems.map((navItem, idx) => {
+          return {
+            title: {
+              label: listItems[idx].name,
+              onClick: () => {
+                listItems[idx].onClick();
+                setOpen(false);
+              },
+            },
+            subtitles: navItem.map((item) => ({
+              label: item.name,
+              onClick: () => {
+                item.onClick();
+                setOpen(false);
+              },
+            })),
+          };
+        })}
+      />
     </header>
   );
 };
