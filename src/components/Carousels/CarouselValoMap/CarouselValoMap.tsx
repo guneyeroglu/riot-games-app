6c626733bb12e127fbe0204bce926d7aeab36680
guneyeroglu <benguneyeroglu@gmail.com>
2023-03-25 00:04:11 +0300

    CarouselValoMap comp started

--- src/components/Carousels/CarouselValoMap/CarouselValoMap.tsx
+++ src/components/Carousels/CarouselValoMap/CarouselValoMap.tsx
@@ -1,7 +1,11 @@
 import styles from './carousel-valo-map.module.scss';
 
 const CarouselValoMap = () => {
-  return <div>CarouselValoMap</div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.wrapper__content}></div>
+    </div>
+  );
 };
 
 export default CarouselValoMap;
