b4d5bc50d37889a28ed3f68b347450b70e0bcc05
Güney <benguneyeroglu@gmail.com>
2024-11-03 19:19:52 +0300

    update: version

--- src/global/constants/urls.ts
+++ src/global/constants/urls.ts
@@ -1,1 +1,1 @@
-export const lolApiVersion: string = '14.2.1'; //* It has to be updated as a new champion is added
+export const lolApiVersion: string = '14.21.1'; //* It has to be updated as a new champion is added
